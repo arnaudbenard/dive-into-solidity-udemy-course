ab748cd6c1a9304bdb36fe44169afbf985094984
Arnaud Benard <arnaudm.benard@gmail.com>
2022-07-20 20:11:11 -0700

    exercise

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,36 +11,33 @@ contract Lottery {
 
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
-        // TODO: require each player to send exactly 0.1 ETH
-        // TODO: append the new player to the players array
+        require(msg.value >= 0.1 ether);
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+    function getBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
-
-        uint256 r = random();
-        address winner;
-
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
-
-        // TODO: append the winner to the gameWinners array
-
-        // TODO: reset the lottery for the next round
-
-        // TODO: transfer the entire contract's balance to the winner
+    function pickWinner() public onlyOwner {
+        require(players.length >= 3, "NOT_ENOUGH_PLAYERS");
+        uint256 index = random() % players.length;
+        address payable winner = payable(players[index]);
+        gameWinners.push(winner);
+        delete players;
+        winner.transfer(address(this).balance);
     }
 
     // helper function that returns a big random integer
